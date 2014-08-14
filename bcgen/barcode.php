488227dba92ccaf654f9254b4af07832bea1e183
Andrew Roth <aroth2@brocku.ca>
2014-08-14 14:23:45 -0400

    minimalist Barcode generator in code128
    Fixed problems with both https://github.com/samt/barcode and
    https://github.com/emberlabs/barcode.
    - Code 128 recognizes pipe ‘|’ character as Type B
    - Fixes conflicting definition for ‘type’ in namespace (appears to work
    if you use defaults, but if you want another ascii set the problem
    appears).
    - adds optional label to top of barcode (using offset).

--- bcgen/barcode.php
+++ bcgen/barcode.php
@@ -1,20 +1,23 @@
 <?php
 
-require_once("../BarcodeBase.php");
-require_once("../Code39.php");
+require_once("BarcodeBase.php");
+require_once("Code128.php");
 
 
 
 $width = isset($_GET['width'])?$_GET['width']:200;
 $height = isset($_GET['height'])?$_GET['height']:40;
 $barcode = isset($_GET['barcode'])?$_GET['barcode']:"No Barcode";
+$label = isset($_GET['label'])?$_GET['label']:"No Label";
 
 
-$barcodeObj = new emberlabs\Barcode\Code39();
+$barcodeObj = new emberlabs\Barcode\Code128();
 $barcodeObj->setData($barcode);
-$barcodeObj->setDimensions($width, $height);
+$barcodeObj->setLabel($label);
+$barcodeObj->setDimensions(400, 60);
 $barcodeObj->setFontSize(5);
 $barcodeObj->enableHumanText(true);
 $barcodeObj->display();
+//$b64 = $barcodeObj->base64();
 
 
